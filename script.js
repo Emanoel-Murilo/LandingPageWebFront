3a19aebd7e09e7e9e5f6da4dbac03280ae120041
Emanoel Murilo <107274726+Emanoel-Murilo@users.noreply.github.com>
2025-05-16 10:33:26 -0300

    finalizaçãoHeader, navbar responsiva

---
+++ script.js
@@ -0,0 +1,13 @@
+// Botão de mostrar opções quando tela esta pequena
+
+const navegacao = document.getElementsByTagName('nav')[0]
+
+function mostrarNav(){
+    if (navegacao.className === 'esconder' ){
+        navegacao.className = '';
+        console.log ("ola")
+    }
+    else {
+        navegacao.className = 'esconder';
+    }
+}
